ff3e3008c262ff41b364a510cf85d2a59afd55cb
seonjo1 <tjdgus9684@gmail.com>
2024-03-10 21:41:06 +0900

    Update main : main문 작성 및 print_contact 함수 추가

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -1,11 +1,49 @@
 #include "PhoneBook.hpp"
 
+void print_contact(Contact &contact)
+{
+	std::cout << "first_name : " << contact.get_first_name() << std::endl;
+	std::cout << "last_name : " << contact.get_last_name() << std::endl;
+	std::cout << "nickname : " << contact.get_nickname() << std::endl;
+	std::cout << "phone_number : " << contact.get_phone_number() << std::endl;
+	std::cout << "darkest_secret : " << contact.get_darkest_secret() << std::endl;
+	std::cout << "\n";
+}
+
 int main()
 {
 	PhoneBook book;
-	for (int i = 0; i < 8; i++)
+
+	while (true)
 	{
-		book.Add();
-		book.Search();
+		std::cout << "Please enter command\n";
+		std::cout << "1. ADD\n";
+		std::cout << "2. SEARCH\n";
+		std::cout << "3. EXIT\n\n";
+		std::string cmd;
+		if (!std::getline(std::cin, cmd))
+		{
+			std::cout << "Program terminated upon receiving EOF\n";
+			std::exit(1);
+		}
+		if (cmd.compare("ADD") == 0)
+			book.ADD();
+		else if (cmd.compare("SEARCH") == 0)
+		{
+			try
+			{
+				Contact& contact = book.SEARCH();
+				print_contact(contact);
+			}
+			catch(const std::string& e)
+			{
+				std::cerr << e;
+			}
+		}
+		else if (cmd.compare("EXIT") == 0)
+			book.EXIT();
+		else
+			std::cout << "This is not a valid command\n\n";
 	}
+	return (0);
 }
