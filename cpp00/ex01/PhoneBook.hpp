832a1b6b7fe3eadd302a6c38f305236338a2e5cf
seonjo1 <tjdgus9684@gmail.com>
2024-03-10 18:58:14 +0900

    Update Phonebook : 소멸자, 복사생성자 삭제

--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -8,11 +8,9 @@ private:
 	int	idx;
 public:
 	PhoneBook();
-	// ~PhoneBook();
-	// PhoneBook(const PhoneBook &copy);
 	void Add();
 	void Search();
 	void Exit();
 	void contact_info(int idx, Contact &contact);
-	void print_info(const std::string &info, int len);
+	void print_info(std::string info, int len);
 };
