594c24918161927270ea12bab193e18a313ed573
seonjo1 <tjdgus9684@gmail.com>
2024-03-10 18:59:15 +0900

    Update PhoneBook : print_info 함수 const 와 & 재거 및  search 뒷부분 틀만 만듬 (주석처리)

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -21,7 +21,7 @@ void PhoneBook::Add()
 	if (size < 8) size++;
 }
 
-void PhoneBook::print_info(const std::string &info, int len)
+void PhoneBook::print_info(std::string info, int len)
 {
 	int i = 0;
 	int j = 0;
@@ -49,7 +49,26 @@ void PhoneBook::Search()
 	std::cout << "--------------------------------------------\n";
 	for (int i = 0; i < size; i++)
 		contact_info(i, book[i]);
-	std::cout << "--------------------------------------------\n";
+	std::cout << "--------------------------------------------\n\n";
+	// int idx;
+	// while (true)
+	// {
+	// 	std::cout << "Index : ";
+	// 	std::cin >> idx;
+	// 	std::cout << idx << std::endl;
+	// 	if (!(idx >= 0 && idx < size))
+	// 	{
+	// 		std::cin.clear();
+	// 		std::cout << "Please re-enter the index\n";
+	// 		continue ;
+	// 	}
+	// 	std::cout << "first_name : " << book[idx].get_first_name() << std::endl;
+	// 	std::cout << "last_name : " << book[idx].get_last_name() << std::endl;
+	// 	std::cout << "nickname : " << book[idx].get_nickname() << std::endl;
+	// 	std::cout << "phone_number : " << book[idx].get_phone_number() << std::endl;
+	// 	std::cout << "darkest_secret : " << book[idx].get_darkest_secret() << std::endl;
+	// 	break ;
+	// }
 }
 
 void PhoneBook::Exit()
